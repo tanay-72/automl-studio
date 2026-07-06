b52742e1a4f86c68c11f1bce0c85ff4d4b0b2991
Tanay Singh Chauhan <tanays.chauhan.cer24@itbhu.ac.in>
2026-07-06 13:55:49 +0530

    Test CORS

--- backend/app/config.py
+++ backend/app/config.py
@@ -34,5 +34,8 @@ CV_FOLDS = 5
 FRONTEND_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+
     "https://automl-studio-eight.vercel.app",
+
+    "https://automl-studio-git-main-tanay-72s-projects.vercel.app",
 ]
--- backend/main.py
+++ backend/main.py
@@ -21,8 +21,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=FRONTEND_ORIGINS,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
